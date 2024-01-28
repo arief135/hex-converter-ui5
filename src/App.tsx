bc8605939ddb88c8f22b58d32c5d6f5d8f97541d
Arief Pratama <ariefpratama135@gmail.com>
2024-01-28 22:32:51 +0700

    update logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,114 @@
 import {
   Avatar,
   FlexBox,
-  FlexBoxAlignItems,
   FlexBoxDirection,
   FlexBoxJustifyContent,
-  Input,
-  InputPropTypes,
-  Label,
   Link,
   LinkDesign,
-  ShellBar
+  ShellBar,
+  TextArea,
+  TextAreaDomRef,
+  Ui5CustomEvent
 } from '@ui5/webcomponents-react';
 import { useState } from 'react';
 
+
 function App() {
-  const [inputVal, setInputVal] = useState('');
-  const handleInput: InputPropTypes['onInput'] = (e) => {
-    setInputVal(e.target.value ?? '');
-  };
+
+  const [ hexText, setHexText ] = useState('')
+  const [ binaryText, setBinaryText ] = useState('')
+
+  const pad = (text: string, size: int) => {
+    while (text.length < size) text = "0" + text;
+    return text;
+  }
+
+  const onChangeFn = (e: Ui5CustomEvent<TextAreaDomRef, never>) => {
+    const decString = e.target.value as string
+    const decArray = decString.split(',')
+
+    let hexText = ''
+    let binText = ''
+
+    for (let i = 0; i < decArray.length; i++) {
+      const decInt = parseInt(decArray[ i ].trim())
+      let hexStr = decInt.toString(16)
+      let binStr = decInt.toString(2)
+
+      hexStr = pad(hexStr, 2)
+      binStr = pad(binStr, 8)
+
+      if (hexText == '') {
+        hexText = hexStr
+      } else {
+        hexText = hexText + ' ' + hexStr
+      }
+
+      if (binText == '') {
+        binText = binStr
+      } else {
+        binText = binText + ' ' + binStr
+      }
+    }
+
+    setHexText(hexText)
+    setBinaryText(binText)
+  }
+
   return (
     <>
       <ShellBar
-        logo={<img src="/vite.svg" alt={'Vite Logo'} />}
-        primaryTitle="UI5 Web Components for React Template"
+        logo={<img src="/logo.png" alt={'Vite Logo'} />}
+        primaryTitle="Decimal converter"
         profile={<Avatar initials={'UI5'} />}
       />
+
       <FlexBox
-        direction={FlexBoxDirection.Column}
-        justifyContent={FlexBoxJustifyContent.Center}
-        alignItems={FlexBoxAlignItems.Center}
+        direction={FlexBoxDirection.Row}
       >
-        <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
-          Getting Started with UI5 Web Component for React
-        </Link>
-        <Input placeholder="Hello World" onInput={handleInput} value={inputVal} />
-        <Label>{inputVal}</Label>
+
+        <FlexBox
+          direction={FlexBoxDirection.Column}
+          fitContainer={true}
+          justifyContent={FlexBoxJustifyContent.SpaceBetween}
+          style={{ marginRight: '10px' }}>
+
+          <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
+            Decimal (separated by comma)
+          </Link>
+
+          <TextArea
+            style={{ height: '200px' }}
+            onChange={onChangeFn}
+          />
+        </FlexBox>
+
+        <FlexBox
+          direction={FlexBoxDirection.Column}
+          fitContainer={true}
+        >
+
+          <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
+            Hex Values
+          </Link>
+
+          <TextArea
+            style={{ height: '200px' }}
+            readonly={true}
+            value={hexText}
+          />
+
+          <Link href="https://sap.github.io/ui5-webcomponents-react/" target="_blank" design={LinkDesign.Emphasized}>
+            Binary Values
+          </Link>
+
+          <TextArea
+            style={{ height: '200px' }}
+            readonly={true}
+            value={binaryText}
+          />
+        </FlexBox>
+
       </FlexBox>
     </>
   );
